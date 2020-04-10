14bf600e8ef073850e6b1a8ef90d6ec6ff04f771
pocandroid <62979933+pocandroid@users.noreply.github.com>
2020-04-10 16:40:20 +0530

    Update SecondActivity.java

--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SecondActivity extends AppCompatActivity {
-
+private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        
+        textView=(TextView)findViewById(R.id.textView);
+        textView.setTextColor(Color.BLUE);
     }
 }
